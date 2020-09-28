9d26fca28f9a615d6ae7bd9fffc05cdb76d3f8bb
ryan.attard <ryan.attard@mcast.edu.mt>
2020-09-28 13:40:41 +0200

    Renamed Data project to DataAccess Added some solution folders to the respective projects where classes will eventually be created

---
+++ ProductReview.Mvc/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductReview.Mvc.Data;
+
+[assembly: HostingStartup(typeof(ProductReview.Mvc.Areas.Identity.IdentityHostingStartup))]
+namespace ProductReview.Mvc.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
